f178970cde6c7a5151d2f86bff795a1d41dd9d39
Robert Goldsmith <r.s.goldsmith@far-blue.co.uk>
2013-06-06 16:58:10 +0100

    Updated the task to: Work with the xml output from our cloned qunit-reporter-junit runner plugin Allow specifying of a report output filename/path Output progress dots

--- QunitTask.php
+++ QunitTask.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 /*
 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -18,16 +18,19 @@ require_once 'phing/util/DataStore.php';
 * Qunit Task, for running qunit <https://github.com/jquery/qunit> headless with phantomjs <https://github.com/ariya/phantomjs>
 *
 * This class is based on Stefan Priebsch JslintTask
-* 
+*
 * @author Martin Jonsson <martin.jonsson@gmail.com>
 */
 class QunitTask extends Task {
 	private $file = null;
 	private $runner = null;
-	private $haltOnFailure = true;    
+	private $haltOnFailure = true;
 	private $hasErrors = false;
 	private $filesets = array();
-    private $executable = 'phantomjs';
+	private $executable = 'phantomjs';
+	private $reportPath;
+
+	private $resultXml;
 
 	public function setFile(PhingFile $file) {
 		$this->file = $file;
@@ -36,15 +39,19 @@ class QunitTask extends Task {
 	public function setExecutable($executable) {
 		$this->executable = $executable;
 	}
-	
+
 	public function setRunner(PhingFile $runner) {
 		$this->runner = $runner;
 	}
-		
+
 	public function setHaltOnFailure($haltOnFailure) {
 		$this->haltOnFailure = $haltOnFailure;
 	}
-    
+
+	public function setReportPath($path) {
+		$this->reportPath = $path;
+	}
+
 	/**
 	* Create fileset for this task
 	*/
@@ -52,77 +59,76 @@ class QunitTask extends Task {
 		$num = array_push($this->filesets, new FileSet());
 		return $this->filesets[$num-1];
 	}
-  
+
 	public function main() {
 		$this->hasErrors = false;
-		
+		$this->resultXml = '';
+
 		if(!isset($this->file) and count($this->filesets) == 0) {
 			throw new BuildException("Missing either a nested fileset or attribute 'file' set");
 		}
+		if ($this->reportPath == '') {
+			throw new BuildException("No report output file path specified");
+		}
+
 		exec($this->executable . '  --version', $output);
 		if (!preg_match('/\d+\.\d+\.\d+/', implode('', $output))) {
 			throw new BuildException('phantomjs command not found');
 		}
-    
+
+		echo '[';
 		if($this->file instanceof PhingFile) {
+			echo '.';
 			$this->run($this->file->getPath());
 		} else { // process filesets
 			$project = $this->getProject();
-		
+
 			foreach($this->filesets as $fs) {
 				$ds = $fs->getDirectoryScanner($project);
 				$files = $ds->getIncludedFiles();
 				$dir = $fs->getDir($this->project)->getPath();
-				
+
 				foreach($files as $file) {
+					echo '.';
 					$this->run($dir.DIRECTORY_SEPARATOR.$file);
 				}
 			}
 		}
-  
+		echo ']', PHP_EOL;
+
+		if (file_exists($this->reportPath)) {
+			unlink($this->reportPath);
+		}
+		file_put_contents($this->reportPath,'<testsuite name="qunit">' . $this->resultXml . '</testsuite>'); 
 		if ($this->haltOnFailure && $this->hasErrors) throw new BuildException('QUnit tests failed');
-    }
-    
+	}
+
 	public function run($file) {
 		$command = $this->executable . ( $this->runner ? ' "' . $this->runner . '"' : '' ) . ' "' . $file . '"';
 
 		if(!file_exists($file)) {
 			throw new BuildException('File not found: ' . $file);
 		}
-		
+
 		if(!is_readable($file))	{
 			throw new BuildException('Permission denied: ' . $file);
 		}
 
 		$messages = array();
-		exec($command, $messages);		
-		$summary = $messages[sizeof($messages) - 1];
-		
-		$matches = array();
- 		if (preg_match('/(\d+) tests of (\d+) passed, (\d+) failed/', $summary, $matches)) {
-			if ($matches[3] > 0) {
-				$this->hasErrors = true;
-			   	$this->log($matches[3] . ' tests failed on: ' . $file, Project::MSG_ERR);
-				$this->logFailedTests($messages);
-			} else {
-				$this->log($file . ': ' . $matches[0], Project::MSG_INFO);
-			}
-		}
-	}
+		exec($command, $messages);
 
-	private function logFailedTests($messages) {
-		if (!is_array($messages)) {
-			return;
+		$output = implode('', $messages);
+		if (!preg_match('/<testsuites\s.+failures="(.+?)"\s.+?\>(\<testsuite.+\<\/testsuite\>)\<\/testsuites\>/', $output, $matches)) {
+			echo 'No xml report created. Raw output follows:', PHP_EOL;
+			echo implode("\n", $messages);
+			throw new BuildException('No xml report created.');
 		}
-		
-		foreach ($messages as $message) {
- 			$matches = array();
-			if (preg_match('/\((\d+), (\d+), (\d+)\)/', $message, $matches)) {
-				if ($matches[1] > 0) {
-					$this->log('Failed test: ' . $message, Project::MSG_ERR);
-				}
 
-			}
+		$this->resultXml .= $matches[2];
+
+		if ($matches[1]) {
+			$this->hasErrors = true;
 		}
+
 	}
 }
